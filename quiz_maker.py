e983d7906f2510508cbc01211c3c9406e434dfb5
Cory Bass <cory.bass.07@gmail.com>
2020-10-16 16:11:28 -0500

    updated quiz_maker.py

--- quiz_maker.py
+++ quiz_maker.py
@@ -9,48 +9,50 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-    
+
+
 class Quiz(db.Quiz):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(100), unique = False)
-    question = db.Column(db.String(100), unique = False)
-    answerA = db.Column(db.String(100), unique = False)
-    answerB = db.Column(db.String(100), unique = False)
-    answerC = db.Column(db.String(100), unique = False)
-    answerD = db.Column(db.String(100), unique = False)
-    def __init__(self, title, question, answerA, answerB, answerC, answerD):
-        self.title = title
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(100), unique=False)
+    answer_a = db.Column(db.String(100), unique=False)
+    answer_b = db.Column(db.String(100), unique=False)
+    answer_c = db.Column(db.String(100), unique=False)
+    answer_d = db.Column(db.String(100), unique=False)
+
+    def __init__(self, question, answer_a, answer_b, answer_c, answer_d):
         self.question = question
-        self.answerA = answerA
-        self.answerB = answerB
-        self.answerC = answerC
-        self.answerD = answerD
+        self.answer_a = answer_a
+        self.answer_b = answer_b
+        self.answer_c = answer_c
+        self.answer_d = answer_d
+
 
 class QuizSchema(ma.Schema):
     class Meta:
-        fields = ('title', 'question', 'answerA', 'answerB', 'answerC', 'answerD')
+        fields = ('question', 'answer_a', 'answer_b', 'answer_c', 'answer_d')
+
 
 quiz_schema = QuizSchema()
-quiz_schema = QuizSchema(many = True)
+quizes_schema = QuizSchema(many=True)
 
 
 @app.route('/quiz', methods=["POST"])
 def add_quiz():
-    title = request.json['title']
     question = request.json['question']
-    answerA = request.json['answerA']
-    answerB = request.json['answerB']
-    answerC = request.json['answerC']
-    answerD = request.json['answerD']
-    
-    new_quiz = Quiz(question, answerA, answerB, answerC, answerD)
+    answer_a = request.json['answer_a']
+    answer_b = request.json['answer_b']
+    answer_c = request.json['answer_c']
+    answer_d = request.json['answer_d']
+
+    new_quiz = Quiz(question, answer_a, answer_b, answer_c, answer_d)
 
     db.session.add(new_quiz)
     db.session.commit()
 
-    movie = Quiz.query.get(new_quiz.id)
+    quiz = Quiz.query.get(new_quiz.id)
 
-    return quiz_schema.jsonify(movie)
+    return quiz_schema.jsonify(quiz)
+
 
 @app.route("/quizzes", methods=["GET"])
 def get_quizzes():
@@ -58,36 +60,40 @@ def get_quizzes():
     result = quiz_schema.dump(all_quizzes)
     return jsonify(result)
 
-@app.route("/quizzes/<id>", methods=["GET"])
+
+@app.route("/quiz/<id>", methods=["GET"])
 def get_quiz(id):
-    movie = Quiz.query.get(id)
-    return Quiz_schema.jsonify(quiz)
+    quiz = Quiz.query.get(id)
+    return quiz_schema.jsonify(quiz)
 
-@app.route("/quizzes/<id>", methods =["PUT"])
+
+@app.route("/quiz/<id>", methods=["PUT"])
 def quiz_update(id):
-    title = Quiz.query.get(id)
+    quiz = Quiz.query.get(id)
     question = request.json['question']
-    answerA = request.json['year']
-    answerB = request.json['rating']
-    answerC = request.json['genre']
-    answerD = request.json['starring']
-    quiz.title = title
-    quiz.question = question
-    quiz.answerA = answerA
-    quiz.answerB = answerB
-    quiz.answerC = answerC
-    quiz.answerD = answerD
+    answer_a = request.json['a']
+    answer_b = request.json['b']
+    answer_c = request.json['c']
+    answer_d = request.json['d']
 
-@app.route("/quiz/<id>", methods =["DELETE"])
+    quiz.question = question
+    quiz.answer_a = answer_a
+    quiz.answer_b = answer_b
+    quiz.answer_c = answer_c
+    quiz.answer_d = answer_d
+
+    db.session.commit()
+    return quiz_schema.jsonify(quiz)
+
+
+@app.route("/quiz/<id>", methods=["DELETE"])
 def quiz_delete(id):
     quiz = Quiz.query.get(id)
     db.session.delete(quiz)
     db.session.commit()
-    return quiz_schema.jsonify(quiz)
 
-    db.session.commit()
     return quiz_schema.jsonify(quiz)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
